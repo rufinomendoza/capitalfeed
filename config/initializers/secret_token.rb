3747b1583b3d186b3abe2c418126a9615bc77801
rufinomendoza <ThinkPadT400@Mobile>
2013-08-08 11:11:33 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capitalfeed::Application.config.secret_token = '7cf2beab005a20e5751c3e348330cdb562e8cc25308e67d9387e309ec485a15f369b6958cf49b58b4500994b3ef9acc9859212b604b0b170f5322a025b1ae7a0'
