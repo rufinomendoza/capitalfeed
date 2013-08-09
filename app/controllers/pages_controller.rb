e5817fd56ff64e9f47a6f7c98c9a0629bff0d0ec
rufinomendoza <ThinkPadT400@Mobile>
2013-08-09 02:28:30 -0400

    Remove update RSS feeds upon Articles page load

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,19 +6,6 @@ class PagesController < ApplicationController
   end
 
   def articles
-    feeds = [
-      'http://www.economist.com/feeds/print-sections/69/leaders.xml',
-      'http://edge.org/feed',
-      'http://capitalmusings.com/feed',
-      'http://bhorowitz.com/feed/',
-      'http://feeds.businesswire.com/BW/Automotive_News-rss',
-      'http://www.foreignpolicy.com/node/feed',
-      'http://www.ft.com/rss/lex',
-      'http://www.kforcegov.com/NightWatch/rss.ashx'
-    ]
-    feeds.each do |feed|
-      FeedEntry.update_from_feed(feed)
-    end
     @articles = FeedEntry.order("published_at desc").page(params[:page]).per_page(20)
 
     respond_to do |format|
