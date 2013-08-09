c41c7e89253cec0b9e16d24587a7088a923367bc
rufinomendoza <ThinkPadT400@Mobile>
2013-08-09 01:13:09 -0400

    Change formatting of pagination along with a few other formatting tweaks

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,7 +13,8 @@ class PagesController < ApplicationController
       'http://bhorowitz.com/feed/',
       'http://feeds.businesswire.com/BW/Automotive_News-rss',
       'http://www.foreignpolicy.com/node/feed',
-      'http://www.ft.com/rss/lex'
+      'http://www.ft.com/rss/lex',
+      'http://www.kforcegov.com/NightWatch/rss.ashx'
     ]
     feeds.each do |feed|
       FeedEntry.update_from_feed(feed)
