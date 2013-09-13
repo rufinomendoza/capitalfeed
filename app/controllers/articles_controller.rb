e431cba75c6185fcbf98bc7defa70f15748d273e
Rufino Mendoza <rlmendoza@aol.com>
2013-09-12 21:07:28 -0400

    Essentially remove boolean from articles#index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,13 +33,13 @@ class ArticlesController < ApplicationController
 
   def index
     if params[:tag]
-      if Article.tagged_with(params[:tag]).where(:published_at).exists?
+      if Article.tagged_with(params[:tag])#.where(:published_at).exists?
         @articles = Article.tagged_with(params[:tag])#.sort_by!{|article| article.published_at}.reverse!
       else
         @articles = Article.tagged_with(params[:tag])
       end
     else
-      if Article.where(:published_at).exists?
+      if Article.all#.where(:published_at).exists?
         @articles = Article.all#.sort_by!{|article| article.published_at}.reverse!
       else
         @articles = Article.all
