07799f3102ebbe8c74b8cfa57d4c018acc64d105
Rufino Mendoza <rlmendoza@aol.com>
2013-09-25 13:25:20 -0400

    Add dependent destroy to Tagging Model

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,6 +1,6 @@
 class Tagging < ActiveRecord::Base
-  belongs_to :tag
-  belongs_to :article
+  belongs_to :tag, dependent: :destroy
+  belongs_to :article, dependent: :destroy
   # attr_accessible :title, :body
   attr_accessible :tag_id, :article_id
 end
