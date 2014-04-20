5c9f6f823a38383f15a5fb65756b7ffa04a6f856
Rufino Mendoza <rlmendoza@aol.com>
2014-04-20 19:47:01 -0400

    Add Header Image Scroll and Stylesheets from previous commit

---
+++ app/assets/javascripts/header_image_scroll.js
@@ -0,0 +1,16 @@
+// Cool header image scroll
+$(window).scroll(function(e){
+  if ($(window).width() > 800) {
+    $('.header').css({
+      'bottom' : -($(this).scrollTop()/3)+"px",
+    }); 
+    var thisdist = $(this).scrollTop();
+    var headerheight = $(".header").outerHeight();
+    $('.blog-info').css({
+      'opacity' : (1 - thisdist/headerheight)
+    }); 
+  } else {
+    $('.header').css({'bottom' : 'auto'});  
+    $('.blog-info').css({'opacity' : "1" });
+  }
+});
