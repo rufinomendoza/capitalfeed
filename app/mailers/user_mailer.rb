a7faf28a6ce693e9e211f695678d883ab824a49c
Rufino Mendoza <rlmendoza@aol.com>
2014-04-28 20:49:04 -0400

    Make sure chronology is descending in the daily briefing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,18 +6,12 @@ class UserMailer < ActionMailer::Base
 
     time_range = (Time.now-1.day)..Time.now
 
-    @top_articles = Article.where(published_at: time_range).where(:category => ["top", "cm"]).order("published_at desc")
-    @wire_articles = Article.where(published_at: time_range).where(:category => ["wire"]).order("published_at desc")
-    
-    @top_articles = @top_articles.sort_by!{|article| article.published_at}.reverse!.first(10)
-    if @top_articles.class == Article
-      @top_articles = [@top_articles]
-    end
+    # Database lookup
+    @politics_articles = Article.tagged_with("Politics").where(published_at: time_range).order("published_at desc") unless Tag.find_by_name("Politics").nil?
+    @business_articles = Article.tagged_with("Business").where(published_at: time_range).order("published_at desc") unless Tag.find_by_name("Business").nil?
+    @technology_articles = Article.tagged_with("Technology").where(published_at: time_range).order("published_at desc") unless Tag.find_by_name("Technology").nil?
 
-    @politics_articles = Article.tagged_with("Politics").where(published_at: time_range).order("published_at desc").first(10) unless Tag.find_by_name("Politics").nil?
-    @business_articles = Article.tagged_with("Business").where(published_at: time_range).order("published_at desc").first(10) unless Tag.find_by_name("Business").nil?
-    @technology_articles = Article.tagged_with("Technology").where(published_at: time_range).order("published_at desc").first(10) unless Tag.find_by_name("Technology").nil?
-
+    # If only one article, make sure it is an array
     if @politics_articles.class == Article
       @politics_articles = [@politics_articles]
     end
@@ -30,10 +24,24 @@ class UserMailer < ActionMailer::Base
       @technology_articles = [@technology_articles]
     end
 
+    # Making sure they are properly arranged in reverse chronology
+    @politics_articles = @politics_articles.sort_by!{|article| article.published_at}.reverse!.first(10)
+    @business_articles = @business_articles.sort_by!{|article| article.published_at}.reverse!.first(10)
+    @technology_articles = @technology_articles.sort_by!{|article| article.published_at}.reverse!.first(10)
 
+    # Currently disabled
+    # @top_articles = Article.where(published_at: time_range).where(:category => ["top", "cm"]).order("published_at desc")
+    # @wire_articles = Article.where(published_at: time_range).where(:category => ["wire"]).order("published_at desc")
 
-    @wire_articles = @wire_articles.sort_by!{|article| article.published_at}.reverse!.first(10)
-
+    # if @top_articles.class == Article
+    #   @top_articles = [@top_articles]
+    # end
+    # if @wire_articles.class == Article
+    #   @wire_articles = [@wire_articles]
+    # end
+    
+    # @top_articles = @top_articles.sort_by!{|article| article.published_at}.reverse!.first(10)
+    # @wire_articles = @wire_articles.sort_by!{|article| article.published_at}.reverse!.first(10)
 
     mail :to => user.email, :subject => "Capital Musings Daily Briefing", :from => "Capital Musings"
   end
